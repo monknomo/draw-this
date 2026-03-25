8de8347bef4379c423e7a92fa00bb35604b925b1
Gunnar Gissel <ggissel@alumni.cmu.edu>
2026-03-24 17:56:04 -0700

    feat: add tools registry index (src/tools/index.ts)

---
+++ src/tools/index.ts
@@ -0,0 +1,21 @@
+// src/tools/index.ts
+import type { Tool } from '../types'
+import { pencil } from './pencil'
+import { drips } from './drips'
+import { stamp } from './stamp'
+import { bubbles } from './bubbles'
+import { eraser } from './eraser'
+import { oops } from './oops'
+import { nuke } from './nuke'
+import { bucket } from './bucket'
+
+export const tools: Record<string, Tool> = {
+  pencil,
+  drips,
+  stamp,
+  bubbles,
+  eraser,
+  oops,
+  nuke,
+  bucket,
+}
